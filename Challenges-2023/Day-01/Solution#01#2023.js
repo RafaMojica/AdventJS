a57f76fa271481eaa7fed2f616045c9c23ffc5eb
Rafael Mojica <rafael.mojica27@gmail.com>
2023-12-03 00:30:37 -0500

    Challenge solution day 1

--- Challenges-2023/Day-01/Solution#01#2023.js
+++ Challenges-2023/Day-01/Solution#01#2023.js
@@ -1,1 +1,9 @@
-function findFirstRepeated(gifts) {}
+function findFirstRepeated(gifts) {
+  let dictionary = {};
+
+  for (const giftId of gifts) {
+    if (dictionary[giftId]) return giftId;
+    dictionary[giftId] = "gift";
+  }
+  return -1;
+}
