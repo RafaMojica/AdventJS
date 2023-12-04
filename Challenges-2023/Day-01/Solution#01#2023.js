92c9a29f26ad0fb7e9fb380934a71d2bb4975c57
Rafael Mojica <rafael.mojica27@gmail.com>
2023-12-04 12:23:41 -0500

    Added more solutions day 01

--- Challenges-2023/Day-01/Solution#01#2023.js
+++ Challenges-2023/Day-01/Solution#01#2023.js
@@ -1,3 +1,4 @@
+// Solutions whit Object
 function findFirstRepeated(gifts) {
   let dictionary = {};
 
@@ -6,4 +7,26 @@ function findFirstRepeated(gifts) {
     dictionary[giftId] = "gift";
   }
   return -1;
+}
+
+// Solution whit Set
+function findFirstRepeated(gifts) {
+  let dictionary = new Set();
+
+  for (const giftId of gifts) {
+    if (dictionary.has(giftId)) return giftId;
+    dictionary.add(giftId);
+  }
+  return -1;
+}
+
+// Solution whit find
+function findFirstRepeated(gifts) {
+  return gifts.find((gift, index) => gifts.indexOf(gift) !== index) ?? -1;
+}
+
+// Solution whit filter
+function findFirstRepeated(gifts) {
+  const repeated = gifts.filter((gift, i) => gifts.indexOf(gift) !== i);
+  return repeated.length > 0 ? repeated[0] : -1;
 }
