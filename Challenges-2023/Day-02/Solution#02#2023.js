b34a1a9ccb4bbf44151396978e27af71e3af00e2
Rafael Mojica <rafael.mojica27@gmail.com>
2023-12-04 14:45:46 -0500

    Solution challenger day 02

--- Challenges-2023/Day-02/Solution#02#2023.js
+++ Challenges-2023/Day-02/Solution#02#2023.js
@@ -1,1 +1,18 @@
-function second(argument) {}
+// Solutions whit Filter
+function manufacture(gifts, materials) {
+  return gifts.filter((gift) => {
+    const lettersArray = gift.split("");
+    return lettersArray.every((letter) => materials.includes(letter));
+  });
+}
+
+// Solutions whit Regex
+function manufacture(gifts, materials) {
+  const regex = new RegExp(`^[${materials}]+${"$"}`);
+  return gifts.filter(regex.test.bind(regex));
+}
+
+function manufacture(gifts, materials) {
+  const regex = new RegExp(`[${materials}]`);
+  return gifts.filter((gift) => !gift.match(regex));
+}
